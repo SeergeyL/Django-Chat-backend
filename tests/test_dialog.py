27059cda6c2874172290232c5d311457a5f87074
SeergeyL <pstydy@inbox.ru>
2021-08-21 14:18:25 +0300

    add test dialog deletion

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -20,3 +20,21 @@ def test_dialog_creation(user1, user2):
     status = Dialog.objects.filter(user1=user2, user2=user1).exists()
 
     assert status, "После добавления в друзья должен быть создан диалог у второго пользователя"
+
+
+@pytest.mark.django_db
+def test_dialog_deletion(user1, user2):
+    qs = Friend.friend_objects.add_friend(user1, user2)
+    qs.accept()
+    Friend.friend_objects.delete_friend(user1, user2)
+
+    dialog = Dialog.objects.get(user1=user1, user2=user2)
+
+    assert not dialog.active, "После удаления из друзей атрибут active должен быть False"
+
+    dialog = Dialog.objects.get(user1=user2, user2=user1)
+
+    assert not dialog.active, "После удаления из друзей атрибут active должен быть False"
+
+
+
